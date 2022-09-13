8e18ce914724cd605e9b8856889d704a05867a63
Heytor Cavalcanti <heytor01@live.com>
2022-09-12 22:51:09 -0300

    feat: added drg response verify to concata all pages data

--- src/commons/providers/drg.provider.ts
+++ src/commons/providers/drg.provider.ts
@@ -1,7 +1,7 @@
 import { auth, api } from "@/commons/services/drg";
 
 import { ISearchDataRequest } from "@/commons/interfaces/drg/ISearchDataRequest";
-import { ISearchDataResponse } from "@/commons/interfaces/drg/ISearchDataResponse";
+import { DataItem, ISearchDataResponse } from "@/commons/interfaces/drg/ISearchDataResponse";
 
 const generateToken = async () => {
   try {
@@ -20,7 +20,9 @@ const generateToken = async () => {
   }
 };
 
-const handleSearchData = async (params: ISearchDataRequest) => {
+const handleSearchData = async (params: ISearchDataRequest): Promise<ISearchDataResponse> => {
+  let dataItems: DataItem[] = [];
+
   try {
     const token = await generateToken();
   
@@ -30,7 +32,23 @@ const handleSearchData = async (params: ISearchDataRequest) => {
       { headers: { Authorization: `Bearer ${token}` } }
     );
 
-    return data;
+    dataItems = data.items;
+
+    const pages = Math.ceil(data.total / 100);
+
+    if (pages > 1) {
+      for (let page = 2; page <= pages; page++) {
+        const { data: { items } } : { data: ISearchDataResponse } = await api.post(
+          '/search',
+          { ...params, page },
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        
+        items.forEach(item => dataItems.push(item));
+      }
+    }
+
+    return { total: data.total, items: dataItems };
   } catch (error: any) {
     console.log(error);
     throw { message: error.message }
